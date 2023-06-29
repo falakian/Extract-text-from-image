284e3ee1aa61d1333bdd1ab38d9c4af67a50790f
alighafoori <444ghafoori@gmail.com>
2023-06-29 23:02:23 +0330

    fix : modify the writing-output-to-binary-file

--- writing-output-to-binary-file.v
+++ writing-output-to-binary-file.v
@@ -1,17 +1,13 @@
-module writing_output_to_binary_file(input reg result[height:0][width:0]);
+module writing_output_to_binary_file(input reg result[height*width*8:0);
 parameter width = 1920;
 parameter height = 1080;
-integer i , j;
+integer i ;
 initial 
     begin
     $fopen("binary-output.bin");
-    for (i=0; i<height; i = i+1) 
+    for (i=0; i<height*width*8; i= i+1) 
         begin
-            for (j=0; j<width; j=j+1) 
-                begin
-                    $fwrite("binary-output.bin", "%h ",result[i][j]);
-                end
-            $fwrite("binary-output.bin", "\n");
+            $fwrite("binary-output.bin", "%h ",result[i][j]);
         end
     $fclose();
     end
